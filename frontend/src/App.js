531a79d77b797a70e1fbdc3470b5758b5b486506
Bart Cant <bart.cant@gmail.com>
2020-06-16 16:10:06 -0400

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,66 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import Auth from '@aws-amplify/auth';
-import {
-  Navbar,
-  Nav,
-  NavDropdown,
-  Form,
-  FormControl,
-  Button,
-  Container,
-  Jumbotron,
-} from "react-bootstrap";
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [username, setUsername] = useState('');
-
-  useEffect(() => {
-      setUsername(Auth.user.username);
-  }, []);
-
   return (
-    <Container>
-      <Navbar bg='light' expand='lg'>
-        <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='mr-auto'>
-            <Nav.Link href='#home'>Home</Nav.Link>
-            <Nav.Link href='#link'>Link</Nav.Link>
-            <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
-              <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
-              <NavDropdown.Item href='#action/3.2'>
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href='#action/3.4'>
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Form inline>
-            <FormControl type='text' placeholder='Search' className='mr-sm-2' />
-            <Button variant='outline-success'>Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Navbar>
-      <AmplifySignOut />
-
-      <Jumbotron className='mt-5'>
-        <h1>{`Hello ${username}`}</h1>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a simple hero unit, a simple jumbotron-style component for
-          calling extra attention to featured content or information.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
-          <Button variant='primary'>Learn more</Button>
-        </p>
-      </Jumbotron>
-    </Container>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
